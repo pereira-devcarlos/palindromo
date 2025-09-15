bacb5d675447fd87f60c18465e4992e53b6d7ad4
pereira-devcarlos <contato.devcarlos@gmail.com>
2025-09-15 13:51:22 -0300

    Corrigir caminho do arquivo de lista e adicionar arquivo de entrada

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lista.h"
+#include "lista/lista.h"
 
 int main(){
-    
+    FILE *entrada = fopen("../data/entrada.txt", "r"); // Caminho relativo à pasta src
 
+    if (entrada == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return 1;
+    } else {
+        printf("Sucesso ao abrir o arquivo!\n");
+    }
+
+    // ... código para ler o arquivo ...
+
+    fclose(entrada);
     return 0;
 }
